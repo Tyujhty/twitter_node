2faef277d7b00ad292c885477bc5507164c69468
Fanny Baquey <fanny.baquey@gmail.com>
2023-02-22 11:25:56 +0100

    feat: Mise en place du MVC

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,10 @@
 const router = require('express').Router()
 const tweet = require('./tweet.routes')
-const api = require('./api')
-const Tweet = require('../database/models/Tweet.model')
+const { tweetList } = require('../controllers/tweet.controller')
 
 // This code is setting up a route for the router object. The route is '/tweet' and it is using the tweet object as the handler for the route. This means that when a request is made to the '/tweet' route, the tweet object will be used to handle the request.
 router.use('/tweet', tweet)
-router.use('/api', api)
+router.get('/', tweetList)
 
-router.get('/', (req, res) => {
-
-    Tweet
-        .find({})
-        .exec()
-        .then(tweets => {
-            res.render('tweets/tweet-list', {tweets})
-        })
-})
 
 module.exports = router
--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -1,8 +1,13 @@
+const { createTweet } = require('../controllers/tweet.controller')
+
 // This code is used to create a router object in Express.js. The router object is used to define routes and associated HTTP methods (GET, POST, PUT, DELETE, etc.) that will be used to handle incoming requests. The router object also provides methods for handling middleware, which is code that is executed before a request is handled.
 const router = require('express').Router()
 
-router.get('/new', (req, res) => {
-    res.render('tweets/tweet-form')
-})
+// router.get('/new', (req, res) => {
+//     res.render('tweets/tweet-form')
+// })
+
+router.post('/new', createTweet)
+
 
 module.exports = router
