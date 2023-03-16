a9757cc83089aaabdbbf59328d51c79f34f66816
Fanny Baquey <fanny.baquey@gmail.com>
2023-03-16 11:02:14 +0100

    feat: ajout de la route comment

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const userRoute = require('./user.routes')
 const authRoute = require('./auth.routes')
 const { tweetList } = require('../controllers/tweet.controller')
 const { ensureAuthenticated } = require('../config/security.config')
+const commentRoute = require('./comment.routes')
 
 router.use('/tweet', ensureAuthenticated, tweetRoute)
 router.use('/user', userRoute)
 router.use('/auth', authRoute)
+router.use('/comment', ensureAuthenticated, commentRoute)
 
 router.get('/', tweetList)
 
